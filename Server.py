c55926f17eabcfef9c16e5b1b55daaa32098bac7
Hesham243 <heshamahmed243@gmail.com>
2023-12-29 22:52:09 +0300

    convert the program into object oriented + add a user friendly data formatting using tabulate package

--- Server.py
+++ Server.py
@@ -2,151 +2,176 @@ import socket
 import threading
 import requests
 import json
+from tabulate import tabulate
 
 
-def api_response(ICAO):
-    params = {
-                'access_key': '0e5eb992a53abb61b4ec5423713fc090',
-                'arr_icao': ICAO,
-                'limit': 100
-            }
+class Server:
+    def __init__(self, host, port):
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.bind((host, port))
+        self.serverSocket.listen(3)
+        print('\n<<<<<<<<<  ----[Server is Online]----  >>>>>>>>>>')
+        self.threads = []
+        self.names = []
+
+
+    def api_response(self, ICAO):
+        params = {
+            'access_key': '0e5eb992a53abb61b4ec5423713fc090',
+            'arr_icao': ICAO,
+            'limit': 100
+        }
+
+
+        print("\nGetting the data from the API. Please wait...")
+        api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
+
+        with open("Group_2.json", "w") as file:
+            json.dump(api_result.json(), file, indent=4)
+
+        print("\nData has been stored in file called [ Group_2.json ]")
+        api_data = json.load(open('Group_2.json'))
+        return api_data
     
-    print("Getting the data from the API. PLease wait... \n")
-    api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
 
-    with open("Group2.json", "w") as file:
-        json.dump(api_result.json(), file, indent=4)
-    
-    print("Data has been stored in the file [ Group 2.json ]\n")
-    api_data = json.load(open('Group2.json'))
-    return api_data
-
-def start_connection(socketActive, api_data):
-    clientName = socketActive.recv(1024).decode('utf-8')
-    names.append(clientName)
-    print("The Client [",clientName,"] connected to the Server successfully.\n")
-    
-    while True:
-        try:
-            Option = socketActive.recv(1024).decode('utf-8')
-            Data_Api= json.load(open('Group2.json'))
-
-            if Option == '1':
-                ####### start All_Arrived_Flights
-                Data_user = ''
-                for Arrived in Data_Api['data']:
-                    if Arrived ['flight_status'] == 'landed':
-                        Data_user +=str([' Flight IATA: ',Arrived['flight']["iata"],', Departure Airport: ',Arrived['departure']['airport'],
-                        ', Arrival Time: ',Arrived['arrival']['estimated'],', Arrival Terminal Number: ',Arrived['flight']['number'],
-                        ', Terminal: ',Arrived['arrival']['terminal'],', Gate: ',Arrived['arrival']['gate']
-                        ])
-                socketActive.sendall((Data_user).encode('utf-8'))
-                print(clientName, ' >>>> Selected Option is [',Option,']')
+    def display_table(self, socket_active, headers, data):
+        table = tabulate(data, headers=headers, tablefmt='grid')
+        socket_active.sendall(table.encode('utf-8'))
 
 
-            elif Option == '2':
-                ####### start All_Delayed_Flights
-                Data_user = ''
-                for Arrived in Data_Api['data']:
-                    if Arrived ['flight_status'] == 'scheduled':
-                        Data_user +=str(['flight IATA: ',Arrived['flight']["iata"],', Departure Airport: ',Arrived['departure']['airport'],
-                        ', Original Departure Time: ',Arrived['departure']['timezone'],', Estimated Time of Arrival: ',Arrived['arrival']['estimated'],
-                        ', Arrival Terminal: ',Arrived['arrival']['terminal'],', Delay: ',Arrived['arrival']['delay'],
-                        ', Arrival Gate: ',Arrived['arrival']['gate']
-                        ])
-                socketActive.sendall((Data_user).encode('utf-8'))
-                print(clientName, ' >>>> Selected Option is [',Option,']')
+    def start_connection(self, socket_active, api_data):
+        client_name = socket_active.recv(1024).decode('utf-8')
+        self.names.append(client_name)
+        print("\nThe Client [", client_name, "] Connected To The Server Successfully.\n")
 
+        while True:
+            try:
+                option = socket_active.recv(1024).decode('utf-8')
+                data_api = json.load(open('Group_2.json'))
 
-            elif Option == '3':
-                ###### start All_Flights_Coming_From_A_Specific_City
-                Data_user = ''
-                IATA = socketActive.recv(8192).decode('utf-8')
-                if len(IATA) < 3:
-                    print(f"\nInvalid IATA code received from {clientName}. Disconnecting.")
-                    socketActive.close()
-                    break
-
-                for Flight in Data_Api['data']:
-                    if Flight['departure']['iata'] == IATA:
-                        Data_user +=str(['Flight IATA Code: ',Flight['flight']['iata'], ',Departure Airport: ', Flight['departure']['airport'],
-                                         ', Original Departure Time: ',Flight['departure']['timezone'], ',Eastimated Arrival Time: ',Flight['arrival']['estimated'],
-                                         ', Departure Gate: ',Flight['departure']['gate'], ', Arrival Gate: ', Flight['arrival']['gate'],
-                                         ', Status: ',Flight['flight_status']
-                                         ])
-
-                socketActive.sendall((Data_user).encode('utf-8'))
-                print(clientName, ' >>>> Selected Option is [',Option,']')
-            
-
-            elif Option == '4':
-                ###### start Details_Of_A_Particular_Flight:
-                Data_user = ''
-                flight_iata = socketActive.recv(8192).decode('utf-8')
+                if option == '1':
+                    data_user = []
+                    for arrived in data_api['data']:
+                        if arrived['flight_status'] == 'landed':
+                            data_user.append([arrived["flight"]["iata"],
+                                             arrived["departure"]["airport"],
+                                             arrived["arrival"]["estimated"],
+                                             arrived["flight"]["number"],
+                                             arrived["arrival"]["terminal"],
+                                             arrived["arrival"]["gate"]])
+                    
+                    headers = ['Flight IATA', 'Departure Airport', 'Arrival Time', 'Arrival Terminal Number', 'Terminal', 'Gate']
+                    self.display_table(socket_active, headers, data_user)
+                    print('Client [' ,client_name, '] ==> Selected Option [', option, ']')
                 
-                if len(flight_iata) < 3:
-                    print(f"\nInvalid flight IATA code received from {clientName}. Disconnecting.")
-                    socketActive.close()
+
+                elif option == '2':
+                    data_user = []
+                    for arrived in data_api['data']:
+                        if arrived['flight_status'] == 'scheduled':
+                            data_user.append([arrived["flight"]["iata"],
+                                              arrived["departure"]["airport"],
+                                              arrived["departure"]["timezone"],
+                                              arrived["arrival"]["estimated"],
+                                              arrived["arrival"]["terminal"],
+                                              arrived["arrival"]["delay"],
+                                              arrived["arrival"]["gate"]])
+                    
+                    headers = ['Flight IATA', 'Departure Airport', 'Original Departure Time', 'Estimated Arrival Time', 'Arrival Terminal', 'Delay', 'Arrival Gate']
+                    self.display_table(socket_active, headers, data_user)
+                    print('Client [' ,client_name, '] ==> Selected Option [', option, ']')
+
+
+                elif option == '3':
+                    data_user = []
+                    iata = socket_active.recv(8192).decode('utf-8').upper()
+                    if len(iata) < 3:
+                        print(f"\nInvalid IATA code received from [ {client_name} ]. Disconnecting.\n")
+                        socket_active.close()
+                        break
+
+                    for flight in data_api['data']:
+                        if flight['departure']['iata'] == iata:
+                            data_user.append([flight["flight"]["iata"],
+                                             flight["departure"]["airport"],
+                                             flight["departure"]["timezone"],
+                                             flight["arrival"]["estimated"],
+                                             flight["departure"]["gate"],
+                                             flight["arrival"]["gate"],
+                                             flight["flight_status"]])
+                    
+                    headers = ['Flight IATA Code', 'Departure Airport', 'Original Departure Time', 'Estimated Arrival Time', 'Departure Gate', 'Arrival Gate', 'Status']
+                    self.display_table(socket_active, headers, data_user)
+                    print('Client [' ,client_name, '] ==> Selected Option [', option, ']')
+                
+
+                elif option == '4':
+                    data_user = []
+                    flight_iata = socket_active.recv(8192).decode('utf-8').upper()
+                    if len(flight_iata) < 3:
+                        print(f"\nInvalid IATA code received from [ {client_name} ]. Disconnecting.\n")
+                        socket_active.close()
+                        break
+
+                    for flight in data_api['data']:
+                        if flight['flight']['iata'] == flight_iata:
+                            data_user.append([flight["flight"]["iata"],
+                                              flight["departure"]["airport"],
+                                              flight["departure"]["gate"],
+                                              flight["departure"]["terminal"],
+                                              flight["arrival"]["airport"], 
+                                              flight["arrival"]["gate"], 
+                                              flight["arrival"]["terminal"],
+                                              flight["flight_status"],
+                                              flight["departure"]["scheduled"],
+                                              flight["arrival"]["scheduled"]])
+                    
+                    headers = [ 'Flight IATA Code', 'Departure Airport', 'Departure Gate', 'Departure Terminal', 'Arrival Airport', 'Arrival Gate', 'Arrival Terminal', 'Status', 'Scheduled Departure Time', 'Scheduled Arrival Time']
+                    self.display_table(socket_active, headers, data_user)
+                    print('Client [' ,client_name, '] ==> Selected Option [', option, ']')
+
+
+                elif option == '5':
+                    data_user = "\nYou quit. Thanks For Your Participating."
+                    socket_active.sendall(data_user.encode('utf-8'))
+                    print('Client [' ,client_name, '] ==> Selected Option [', option, ']')
+                    print("\nThe Client [" , client_name, "] Disconnected from the Server.\n")
                     break
 
-                for Flight in Data_Api['data']:
-                    if Flight['flight']['iata'] == flight_iata:
-                        Data_user +=str(['Flight IATA Code: ',Flight['flight']['iata'], ', Departure Airport: ',Flight['departure']['airport'],
-                                         ', Departure Gate: ',Flight['departure']['gate'], ', Departure Terminal: ',Flight['departure']['terminal'],
-                                         ', Arrival Airport: ',Flight['arrival']['airport'], ', Arrival Gate: ',Flight['arrival']['gate'],
-                                         ', Arrival Terminal: ',Flight['arrival']['terminal'], ', Status: ',Flight['flight_status'],
-                                         ', Scheduled Departure Time: ',Flight['departure']['scheduled'], ', Scheduled Arrival Time: ',Flight['arrival']['scheduled']
-                                         ])
-                socketActive.sendall((Data_user).encode('utf-8'))
-                print(clientName, ' >>>> Selected Option is [',Option,']')
 
-
-            elif Option == '5':
-                Data_user ="You quit. Thanks for your participating."
-                socketActive.sendall(Data_user.encode('utf-8'))
-                print(clientName, ' >>>> Selected Option is [',Option,']\n')
-                print("\nThe Client [",clientName,"] Disconnected from the Server !")
+            except Exception as e:
+                print(f"\n>>>>> Error processing client request: {e}\n")
+                print("\nThe Client [" ,client_name, "] Disconnected from the Server.\n")
+                self.names.remove(client_name)
+                socket_active.close()
                 break
-                
-        except Exception as e:
-            print(f"Error processing client request: {e}")
-            print("\nThe Client [", clientName, "] Disconnected from the Server !!!")
-            names.remove(clientName)
-            socketActive.close()
-            break
 
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind (('127.0.0.1', 49999))
-serverSocket.listen(3)
-print ('\n<<<<<<<<<  ----[Server is Online]----  >>>>>>>>')
+    def valid_input(self, input_str):
+        return len(input_str) >= 3
 
 
-threads=[]
-names=[]
+    def run(self):
+        while True:
+            icao = input("\nEnter The Airport Code (ICAO): ")
+            if self.valid_input(icao):  # check the validity of the airport code
+                break
 
-def valid_input(input_str):
-    if len(input_str) >= 3:
-        return True
-    else:
-        return False
-   
-while True:
-    ICAO = input("\nEnter the airport code (ICAO): ").upper()
-    if valid_input(ICAO): # check the validity of the airport code
-        break
+        print("\nChosen Airport Code (ICAO) is [" ,icao, "]")
 
-print("\nChosen airport code (icao) is [",ICAO,"]")
+        api_data = self.api_response(icao)
 
-api_data = api_response(ICAO)
+        while True:
+            socket_active, socket_name = self.serverSocket.accept()
+            st = threading.Thread(target=self.start_connection, args=(socket_active, api_data))
+            self.threads.append(st)
+            st.start()
+            if len(self.threads) > 5:
+                break
 
-while True:
-    socketActive, socketName = serverSocket.accept()
-    st = threading.Thread( target=start_connection, args=(socketActive, api_data) )
-    threads.append(st)
-    st.start()
-    if len(threads)>5:
-        break
+        socket_active.close()
 
 
-socketActive.close()
+if __name__ == "__main__":
+    server = Server('127.0.0.1', 49999)
+    server.run()
--- client.py
+++ client.py
@@ -1,101 +1,131 @@
 import socket
+from tabulate import tabulate
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    clientSocket.connect(('127.0.0.1', 49999))
-except Exception as e:
-    print(f"Error connecting to the server: {e}")
-    exit()
-
-clientName = input("Enter client's username here: ")
-clientSocket.sendall(clientName.encode('utf-8'))
+class Client:
+    def __init__(self, host, port):
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
 
 
-def recv_all(clientSocket):
-    data = b''
-    while True:
+    def connect_to_server(self):
         try:
-            part = clientSocket.recv(4096)
-            if not part:
-                raise Exception("Connection closed by the server.")
-            data += part
-            if len(part) < 4096:
-                break
+            self.clientSocket.connect((self.host, self.port))
         except Exception as e:
-            print(f"Error receiving data: {e}")
-            break
-    return data
+            print(f"\n>>>>> Error connecting to the server: {e}")
+            exit()
 
 
-while True:
-
-    try:
-        print("1- Display All arrived flights")
-        print("2- Display All delayed flights")
-        print("3- Display All flights from a specific city")
-        print("4- Display Details of a particular flight")
-        print("5- Quit ")
-        option = input("\nEnter a option number [1-5]: \n")
-        print()
-        clientSocket.sendall(option.encode('utf-8'))
-        
-
-        if   option == '1':
-            data = recv_all(clientSocket)
-            print(data.decode('utf-8'))
+    def send_username(self):
+        client_name = input("\nEnter client's username here: ")
+        self.clientSocket.sendall(client_name.encode('utf-8'))
 
 
-        elif option == '2':
-            data = recv_all(clientSocket) 
-            print(data.decode('utf-8'))
-
-
-        elif option == '3':
-            while True:
-                city_iata = input("Enter the city (iata) here: ").upper()
-                if len(city_iata) >= 3: # check the validity of the city iata
+    def recv_all(self):
+        data = b''
+        while True:
+            try:
+                part = self.clientSocket.recv(4096)
+                if not part:
+                    raise Exception("\n>>>>> Connection closed by the server.")
+                data += part
+                if len(part) < 4096:
                     break
-                else:
-                    print("Invalid city IATA code. Please enter a valid code.")
-            clientSocket.sendall(city_iata.encode('utf-8'))          
-            data = recv_all(clientSocket) 
-            decoded_data = data.decode('utf-8')
+            except Exception as e:
+                print(f"\n>>>>> Error receiving data: {e}")
+                break
+        return data
+    
 
-            if not decoded_data.strip():
-                print("No data found for the specified city. Please try again.")
-            else:
-                print(decoded_data)
+    def menu(self):
+        while True:
+            try:
+                print("1- Display All arrived flights")
+                print("2- Display All delayed flights")
+                print("3- Display All flights from a specific city")
+                print("4- Display Details of a particular flight")
+                print("5- Quit ")
+                option = input("\nEnter a Option Number [1-5]: \n")
+                print()
+                self.clientSocket.sendall(option.encode('utf-8'))
+
+                if option == '1':
+                    data = self.recv_all()
+                    try:
+                        print(data.decode('utf-8'))
+                    except Exception as e:
+                        print(f"\n>>>>> Error displaying table: {e}\n")
+                
+
+                elif option == '2':
+                    data = self.recv_all()
+                    try:
+                        print(data.decode('utf-8'))
+                    except Exception as e:
+                        print(f"\n>>>>> Error displaying table: {e}\n")
 
 
-        elif option == '4':
-            while True: 
-                flight_iata = input("Enter the flight (iata) here: ").upper()
-                if len(flight_iata) >= 3: # check the validity of the flight iata
+                elif option == '3':
+                    while True:
+                        user_input = input("\nEnter the city (iata) here: ").upper()
+                        print()
+                        if len(user_input) >= 3:  # check the validity of the input
+                            break
+                        else:
+                            print("\n>>>>> Invalid city iata entered. Please enter a valid code.")
+                    self.clientSocket.sendall(user_input.encode('utf-8'))
+                    data = self.recv_all()
+                    decoded_data = data.decode('utf-8')
+
+                    try:
+                        if not decoded_data.strip():
+                            print("\n>>>> No data found. Please try again.\n")
+                        else:
+                            print(decoded_data)
+                    except Exception as e:
+                        print(f"\n>>>>> Error displaying table: {e}\n")
+                
+
+                elif option == '4':
+                    while True:
+                        user_input = input("\nEnter the flight (iata) here: ").upper()
+                        print()
+                        if len(user_input) >= 3:  # check the validity of the input
+                            break
+                        else:
+                            print("\n>>>>> Invalid flight iata entered. Please enter a valid code.")
+                    
+                    self.clientSocket.sendall(user_input.encode('utf-8'))
+                    data = self.recv_all()
+                    decoded_data = data.decode('utf-8')
+
+                    try:
+                        if not decoded_data.strip():
+                            print("\n>>>>> No data found. Please try again.\n")
+                        else:
+                            print(decoded_data)
+                    except Exception as e:
+                        print(f"\n>>>>> Error displaying table: {e}\n")
+                
+
+                elif option == '5':
+                    data = self.clientSocket.recv(8192)
+                    print(data.decode('utf-8'))
+                    print()
+                    self.clientSocket.close()
                     break
+
                 else:
-                    print("Invalid flight IATA code. Please enter a valid code.")
-            clientSocket.sendall(flight_iata.encode('utf-8'))
-            data = recv_all(clientSocket) 
-            decoded_data = data.decode('utf-8')
+                    print("\n>>>>> Invalid option entered, Please enter a option from 1 to 5 only: ")
+                    print()
 
-            if not decoded_data.strip():
-                print("No data found for the specified flight. Please try again.")
-            else:
-                print(decoded_data)
-        
+            except Exception as e:
+                print(f"\n>>>>> Unexpected error: {e}\n")
+                break
 
-        elif option == '5':
-            data = clientSocket.recv(8192)
-            print(data.decode('utf-8'))
-            print()
-            clientSocket.close()
-            break
 
-        else:
-            print("\n Invalid option, Please enter a number from 1 to 5 only: ")
-            print()
-        
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-        break
+if __name__ == "__main__":
+    client = Client('127.0.0.1', 49999)
+    client.connect_to_server()
+    client.send_username()
+    client.menu()
